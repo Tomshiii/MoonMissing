acb37425c9172ca9da1ac319389adcae1a0e4614
Phillip <FilupErwinSmith@gmail.com>
2022-01-22 18:37:34 +1100

    ApplicationShell deserializes moon data and launches main app

--- MoonMissing/App.xaml.cs
+++ MoonMissing/App.xaml.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: MoonMissing
+// Project: MoonMissing
+// File Name: App.xaml.cs
+// 
+// Current Data:
+// 2022-01-22 4:11 PM
+// 
+// Creation Date:
+// 2022-01-22 4:01 PM
+
+#endregion
+
+#region usings
+
 using System.Windows;
+
+#endregion
 
 namespace MoonMissing
 {
   /// <summary>
-  /// Interaction logic for App.xaml
+  ///   Interaction logic for App.xaml
   /// </summary>
   public partial class App : Application
   {
+    private void App_OnStartup(object sender, StartupEventArgs e)
+    {
+      ApplicationShell.NewAppShell();
+    }
   }
 }
---
+++ MoonMissing/ApplicationShell.cs
@@ -0,0 +1,78 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: MoonMissing
+// Project: MoonMissing
+// File Name: ApplicationShell.cs
+// 
+// Current Data:
+// 2022-01-22 5:51 PM
+// 
+// Creation Date:
+// 2022-01-22 4:08 PM
+
+#endregion
+
+#region usings
+
+#endregion
+
+#region usings
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using MoonMissing.Models;
+using MoonMissing.Resources;
+using MoonMissing.ViewModels;
+using MoonMissing.Views;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace MoonMissing
+{
+  internal class ApplicationShell
+  {
+    private ApplicationShell()
+    {
+      var moonData = GetMoonData();
+      WriteAsCsv(moonData);
+
+      var window = new MainWindowView { DataContext = new MainWindowViewModel() };
+      window.ShowDialog();
+
+      Environment.Exit(0);
+    }
+
+    public static ApplicationShell NewAppShell()
+    {
+      return new ApplicationShell();
+    }
+
+    private static IEnumerable<MoonData> GetMoonData()
+    {
+      // NOTE: Check moon.json needs to be EmbeddedResource.
+
+      var fileData = Encoding.Default.GetString(EmbeddedData.MoonData);
+      var moonData = JsonConvert.DeserializeObject<List<MoonData>>(fileData);
+
+      return moonData ?? throw new NullReferenceException("Moon Data did not deserialize");
+    }
+
+    private static void WriteAsCsv(IEnumerable<MoonData> moonData)
+    {
+      var csv = new StringBuilder();
+
+      csv.AppendLine("id,moonNumber,kingdom,moonName,,");
+      foreach (var data in moonData)
+      {
+        csv.AppendLine($"{data.Id},{data.MoonNumber},{data.Kingdom.Name},{data.MoonName.Replace(",", "")},,");
+      }
+
+      File.WriteAllText("CSV_MOONS.csv", csv.ToString());
+    }
+  }
+}
