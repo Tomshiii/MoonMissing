0e5a34cffd00ce5fbf1f8047a6793c093367d9e4
Phillip <FilupErwinSmith@gmail.com>
2022-01-22 19:57:14 +1100

    Created ViewModels

--- MoonMissing/ViewModels/MainWindowViewModel.cs
+++ MoonMissing/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: MoonMissing
+// Project: MoonMissing
+// File Name: MainWindowViewModel.cs
+// 
+// Current Data:
+// 2022-01-22 7:55 PM
+// 
+// Creation Date:
+// 2022-01-22 4:13 PM
+
+#endregion
+
+#region usings
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AllOverIt.Extensions;
+using MoonMissing.Models;
+
+#endregion
 
 namespace MoonMissing.ViewModels
 {
   internal class MainWindowViewModel
   {
+    private readonly IReadOnlyList<MoonInfoWindowViewModel> _moonWindowViewModels;
+
+    public MainWindowViewModel(IEnumerable<MoonData> moonData)
+    {
+      _moonWindowViewModels = moonData
+        .OrderBy(x => x.Kingdom.Value)
+        .ThenBy(x => x.MoonNumber)
+        .GroupBy(x => x.Kingdom.Name)
+        .Select(x => new MoonInfoWindowViewModel(x))
+        .AsReadOnlyList();
+    }
   }
 }
---
+++ MoonMissing/ViewModels/MoonInfoWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MoonMissing.Models;
+
+namespace MoonMissing.ViewModels
+{
+  internal class MoonInfoWindowViewModel
+  {
+    private IEnumerable<MoonData> _moonData;
+
+    public MoonInfoWindowViewModel(IEnumerable<MoonData> moonData)
+    {
+      _moonData = moonData;
+    }
+  }
+}
