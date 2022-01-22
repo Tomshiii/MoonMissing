860b05549ed101512c750a985ec76fa340225338
Phillip <FilupErwinSmith@gmail.com>
2022-01-22 18:36:53 +1100

    Added Views

---
+++ MoonMissing/Views/MainWindowView.xaml.cs
@@ -0,0 +1,35 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: MoonMissing
+// Project: MoonMissing
+// File Name: MainWindowView.xaml.cs
+// 
+// Current Data:
+// 2022-01-22 4:13 PM
+// 
+// Creation Date:
+// 2022-01-22 4:12 PM
+
+#endregion
+
+#region usings
+
+using System.Windows;
+
+#endregion
+
+namespace MoonMissing.Views
+{
+  /// <summary>
+  ///   Interaction logic for MainWindowView.xaml
+  /// </summary>
+  public partial class MainWindowView : Window
+  {
+    public MainWindowView()
+    {
+      InitializeComponent();
+    }
+  }
+}
