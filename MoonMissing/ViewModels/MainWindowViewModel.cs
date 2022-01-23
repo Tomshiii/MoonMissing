c77f352cff1017c0f0340fa650f1c981a9ab9353
Phillip <FilupErwinSmith@gmail.com>
2022-01-23 19:35:34 +1100

    Wip

--- MoonMissing/ViewModels/MainWindowViewModel.cs
+++ MoonMissing/ViewModels/MainWindowViewModel.cs
@@ -7,68 +7,27 @@
 // File Name: MainWindowViewModel.cs
 // 
 // Current Data:
-// 2022-01-22 8:34 PM
+// 2022-01-23 7:00 PM
 // 
 // Creation Date:
-// 2022-01-22 4:13 PM
+// 2022-01-23 6:58 PM
 
 #endregion
 
 #region usings
 
-using System;
 using System.Collections.Generic;
-using AllOverIt.Extensions;
-using MoonMissing.Extensions;
-using MoonMissing.Models;
-using MoonMissing.Resources;
-using System.Drawing;
 using System.Linq;
 using System.Windows.Media.Imaging;
+using AllOverIt.Extensions;
+using MoonMissing.Models;
 
 #endregion
 
 namespace MoonMissing.ViewModels
 {
-    public class ImageFactory
-  {
-    private readonly ImageConverter _imageConverter = new();
-    private readonly IReadOnlyDictionary<int, Func<Bitmap>> _registry;
-
-    public ImageFactory()
-    {
-      _registry = new Dictionary<int, Func<Bitmap>>
-      {
-        [0] = () => (Bitmap)_imageConverter.ConvertFrom(LevelImages.Cap)
-      };
-    }
-
-    public BitmapImage Create(int key)
-    {
-      if (_registry.TryGetValue(key, out var value))
-      {
-        if (value is null)
-        {
-          throw new InvalidOperationException();
-        }
-
-        var image = value();
-
-        if (image is null)
-        {
-          throw new InvalidOperationException();
-        }
-
-        return image.ToBitmapImage();
-      }
-
-      throw new InvalidOperationException("Key is not registered");
-    }
-  }
-
   internal class MainWindowViewModel
   {
-    private readonly ImageFactory _imageFactory = new();
     private readonly IReadOnlyList<MoonInfoWindowViewModel> _moonWindowViewModels;
     public BitmapImage Image { get; set; }
 
@@ -80,8 +39,6 @@ namespace MoonMissing.ViewModels
         .GroupBy(x => x.Kingdom.Name)
         .Select(x => new MoonInfoWindowViewModel(x))
         .AsReadOnlyList();
-
-      Image = _imageFactory.Create(0);
     }
   }
 }
