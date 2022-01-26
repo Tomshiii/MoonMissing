1920764601fd2160146d53e508fb3c0569a50de6
Malcolm Smith <malcolm@mjfreelancing.com>
2022-01-26 22:00:01 +1100

    Updated to load JSON and populate the database

--- MoonMissing.Data/Models/KingdomName.cs
+++ MoonMissing.Data/Models/KingdomName.cs
@@ -3,26 +3,25 @@ using System.Runtime.CompilerServices;
 
 namespace MoonMissing.Data.Models
 {
-    //[JsonConverter(typeof(EnrichedEnumJsonConverter<KingdomName>))]
     public sealed class KingdomName : EnrichedEnum<KingdomName>
     {
-        public static readonly KingdomName Mushroom = new(0);
-        public static readonly KingdomName Cap = new(1);
-        public static readonly KingdomName Cascade = new(2);
-        public static readonly KingdomName Sand = new(3);
-        public static readonly KingdomName Lake = new(4);
-        public static readonly KingdomName Wooded = new(5);
-        public static readonly KingdomName Cloud = new(6);
-        public static readonly KingdomName Lost = new(7);
-        public static readonly KingdomName Metro = new(8);
-        public static readonly KingdomName Snow = new(9);
-        public static readonly KingdomName Seaside = new(10);
-        public static readonly KingdomName Luncheon = new(11);
-        public static readonly KingdomName Ruined = new(12);
-        public static readonly KingdomName Bowsers = new(13, "Bowser's");
-        public static readonly KingdomName Moon = new(14);
-        public static readonly KingdomName DarkSide = new(15, "Dark Side");
-        public static readonly KingdomName DarkerSide = new(16, "Darker Side");
+        public static readonly KingdomName Mushroom = new(1);
+        public static readonly KingdomName Cap = new(2);
+        public static readonly KingdomName Cascade = new(3);
+        public static readonly KingdomName Sand = new(4);
+        public static readonly KingdomName Lake = new(5);
+        public static readonly KingdomName Wooded = new(6);
+        public static readonly KingdomName Cloud = new(7);
+        public static readonly KingdomName Lost = new(8);
+        public static readonly KingdomName Metro = new(9);
+        public static readonly KingdomName Snow = new(10);
+        public static readonly KingdomName Seaside = new(11);
+        public static readonly KingdomName Luncheon = new(12);
+        public static readonly KingdomName Ruined = new(13);
+        public static readonly KingdomName Bowsers = new(14, "Bowser's");
+        public static readonly KingdomName Moon = new(15);
+        public static readonly KingdomName DarkSide = new(16, "Dark Side");
+        public static readonly KingdomName DarkerSide = new(17, "Darker Side");
 
         private KingdomName()   // Required for EF serialization
         {
