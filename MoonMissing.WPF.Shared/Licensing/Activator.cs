9d230305ef03d2ef12aa8e004d46c191e71a6998
Phillip <FilupErwinSmith@gmail.com>
2022-01-26 12:41:49 +1100

    Added shared project

---
+++ MoonMissing.WPF.Shared/Licensing/Activator.cs
@@ -0,0 +1,43 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: MoonMissing
+// Project: MoonMissing.WPF.Shared
+// File Name: Activator.cs
+// 
+// Current Data:
+// 2022-01-26 11:11 AM
+// 
+// Creation Date:
+// 2022-01-26 9:59 AM
+
+#endregion
+
+#region usings
+
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Syncfusion.Licensing;
+
+#endregion
+
+namespace MoonMissing.WPF.Shared.Licensing
+{
+  public static class Activator
+  {
+    private static readonly IConfigurationRoot Configuration;
+
+    static Activator()
+    {
+      Configuration = new ConfigurationBuilder()
+        .AddUserSecrets(Assembly.GetCallingAssembly())
+        .Build();
+    }
+
+    public static void ActivateSyncFusion()
+    {
+      SyncfusionLicenseProvider.RegisterLicense(Configuration["Licenses:SyncfusionLicenseKey"]);
+    }
+  }
+}
