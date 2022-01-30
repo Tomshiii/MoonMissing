73b376e9832cd3f13c6b5b7e8ba403b5274fa0d6
Phillip <FilupErwinSmith@gmail.com>
2022-01-30 17:18:50 +1100

    Added comment to script

--- Scripts/copyDb.bat
+++ Scripts/copyDb.bat
@@ -1,5 +1,8 @@
 @echo off
 
+:: Copies the database file from the bin of the deoplyment project to the wpf project.
+:: This is only done for Debug since all Release builds will be manually composed.
+
 echo Copying database
 robocopy "..\MoonMissing.Data.Deploy\bin\Debug\net6.0" "..\MoonMissing\bin\Debug\net6.0-windows" "MoonMissing.db"
 echo Done
